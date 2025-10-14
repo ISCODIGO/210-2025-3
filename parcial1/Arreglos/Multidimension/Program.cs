3c001c3d73270629dfc88d3f19cab14d5f661b15
Enrique Avila <senquevila@gmail.com>
2025-10-14 17:58:56 -0600

    continuacion de arrays

--- parcial1/Arreglos/Multidimension/Program.cs
+++ parcial1/Arreglos/Multidimension/Program.cs
@@ -1,4 +1,4 @@
-﻿// Matriz (array de 2 dimensiones)
+﻿Console.WriteLine("Matriz (array de 2 dimensiones)");
 
 int[,] matrix = new int[2, 3];  // 2 filas y 3 columnas
 
@@ -23,7 +23,7 @@ for (int i = 0; i < matrix.GetLength(0); i++)
     Console.WriteLine();
 }
 
-// Arreglo de 3 dimensiones
+Console.WriteLine("Arreglo de 3 dimensiones");
 int[,,] cubo = new int[3, 3, 3];
 cubo[1, 1, 1] = 5;
 Console.WriteLine("Cantidad de elementos: " + cubo.Length);
@@ -41,6 +41,22 @@ for (int i = 0; i < cubo.GetLength(0); i++)
             Console.Write(", ");
         }
         Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine("Arreglos irregulares");
+int[][] irregulares = new int[3][];
+irregulares[0] = new int[]{1, 2};
+irregulares[1] = new int[]{3, 4, 5};
+irregulares[2] = new int[]{6};
+
+for  (int i = 0; i < irregulares.Length; i++)
+{
+    for (int j = 0; j < irregulares[i].Length; j++)
+    {
+        Console.Write(irregulares[i][j]);
+        Console.Write(", ");
     }
     Console.WriteLine();
 }
