ffa2874a9f08252797e080c5cd49cbfa12be9f65
Enrique Avila <senquevila@gmail.com>
2025-10-22 18:57:29 -0600

    objetos - Cliente

---
+++ parcial2/Objetos/Program.cs
@@ -0,0 +1,15 @@
+﻿class Program 
+{
+    public static void Main(string[] args) {
+        Cliente c01 = new Cliente(
+            "Jose Martinez",
+            "0801199012377",
+            "99991238",
+            "jmartinez@unah.hn"
+        );
+
+        Console.WriteLine("---------");
+        Console.WriteLine(c01);
+        Console.WriteLine(c01.GetTelefono());
+    }
+}
