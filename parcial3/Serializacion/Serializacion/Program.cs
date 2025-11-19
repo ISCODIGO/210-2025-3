54a22fd0388bd3c5863a278afa2b7bfc511cad7c
conjuror1024 <conjuror1024@gmail.com>
2025-11-19 07:27:06 -0600

    Agregar menu

--- parcial3/Serializacion/Serializacion/Program.cs
+++ parcial3/Serializacion/Serializacion/Program.cs
@@ -1,25 +1,48 @@
 ﻿using Serializacion;
 using System.Text.Json;
 
-Libreria libreria = new Libreria("Navarro", 10);
+Console.WriteLine("=== MENÚ ===");
+Console.WriteLine("1. Serializar datos");
+Console.WriteLine("2. Deserializar datos");
+Console.Write("Seleccione una opción (1-2): ");
 
-libreria.AgregarLibro(new Libro("El Quijote de la Mancha", "Cervantes", "209443253", 2024));
-libreria.AgregarLibro(new Libro("El Aleph", "Borges", "204656456", 2020));
+string? opcion = Console.ReadLine();
 
-libreria.ImprimirLibros();
-
-// Serializar: convertir un objeto a un archivo JSON
-Console.WriteLine("Serializar datos...");
-string json = JsonSerializer.Serialize(libreria);
-
-using (StreamWriter writer = new StreamWriter("datos.json"))
+switch (opcion)
 {
-   writer.Write(json);
+   case "1":
+      // Serializar: convertir un objeto a un archivo JSON
+      Libreria libreria = new Libreria("Navarro", 10);
+
+      libreria.AgregarLibro(new Libro("El Quijote de la Mancha", "Cervantes", "209443253", 2024));
+      libreria.AgregarLibro(new Libro("El Aleph", "Borges", "204656456", 2020));
+      libreria.AgregarLibro(new Libro("Cien Años de Soledad", "Gabriel Garcia Marquez", "203453453", 2019));
+
+      Console.WriteLine("Serializando datos...");
+      string json = JsonSerializer.Serialize(libreria);
+      using (StreamWriter writer = new StreamWriter("datos.json"))
+      {
+         writer.Write(json);
+      }
+      Console.WriteLine("Datos serializados correctamente.");
+      break;
+
+   case "2":
+      // Deserializar: convertir el archivo json en un objeto
+      Console.WriteLine("Deserializando datos...");
+      if (File.Exists("datos.json"))
+      {
+         string json2 = File.ReadAllText("datos.json");
+         var lib = JsonSerializer.Deserialize<Libreria>(json2);
+         lib?.ImprimirLibros();
+      }
+      else
+      {
+         Console.WriteLine("El archivo datos.json no existe.");
+      }
+      break;
+
+   default:
+      Console.WriteLine("Opción no válida.");
+      break;
 }
-
-// Deserializar: convertir el archivo json2 en un objeto
-Console.WriteLine("Deserializar datos...");
-string json2 = File.ReadAllText("datos.json");
-var lib = JsonSerializer.Deserialize<Libreria>(json2);
-
-lib?.ImprimirLibros();
