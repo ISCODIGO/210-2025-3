f2d66f47d1873d5cfb716f1f00bc68c2ae4c6f8f
Enrique Avila <senquevila@gmail.com>
2025-11-18 18:55:15 -0600

    Busqueda y Stream

---
+++ parcial3/Archivos/Archivos/Program.cs
@@ -0,0 +1,13 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        string ruta = @"temp\prueba.txt";
+        //File.Create(ruta);
+        File.WriteAllText(ruta, "Hola mundo\nHoy es jueves");
+        File.AppendAllText(ruta, "Mañana es viernes");
+        File.AppendAllLines(ruta, new string[] { "1", "2", "3" });
+        File.AppendAllLines(ruta, new List<string>  { "1", "2", "3" });
+        File.rea
+    }
+}
